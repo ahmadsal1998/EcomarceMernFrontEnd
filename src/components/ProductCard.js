ad20e7028b3ba1a2077ba7d3129d625aa49832ac
ahmadsal1998 <salamea015@gmail.com>
2024-03-26 20:49:25 +0200

    version_1 ecomarce ues react

---
+++ src/components/ProductCard.js
@@ -0,0 +1,120 @@
+import React from "react";
+import ReactStars from "react-rating-stars-component";
+import { Link, useLocation } from "react-router-dom";
+import wish from "../images/wish.svg";
+import watch from "../images/watch.jpg";
+import speaker from "../images/speaker.jpg";
+import prodcompare from "../images/prodcompare.svg";
+import view from "../images/view.svg";
+import addcart from "../images/add-cart.svg";
+
+const ProductCard = (props) => {
+  const { grid } = props;
+  let location = useLocation();
+  return (
+    <>
+      <div
+        className={` ${
+          location.pathname === "/store" ? `gr-${grid}` : "col-3"
+        } `}
+      >
+        <Link to=":id" className="product-card position-relative">
+          <div className="wishlist-icon position-absolute">
+            <button className="border-0 bg-transparent">
+              <img src={wish} alt="wishlist" />
+            </button>
+          </div>
+          <div className="product-image">
+            <img src={watch} className="img-fluid" alt="product img" />
+            <img src={speaker} alt="product img" />
+          </div>
+          <div className="product-details">
+            <h6 className="brand">Havels</h6>
+            <h5 className="product-title">
+              Kids headphones bulk 10 pack multi colored for students
+            </h5>
+            <ReactStars
+              count={5}
+              size={24}
+              value={4}
+              edit={false}
+              activeColor="#ffa700"
+            />
+            <p className={`description ${grid === 12 ? "d-block" : "d-none"}`}>
+              At vero eos et accusamus et lusto odioGign1ss1mos ducxmus qul
+              blanditiis praesentium voluptatum deleniti atque corrupti quos
+              dolores et quas molestias excepturi sint occaecati cupiditate non
+              provident, similique sunt.
+            </p>
+            <p className="price">100.00$</p>
+          </div>
+          <div className="action-bar position-absolute">
+            <div className="d-flex flex-column gap-15">
+              <Link>
+                <img src={prodcompare} alt="compare" />
+              </Link>
+              <Link>
+                <img src={view} alt="view" />
+              </Link>
+              <Link>
+                <img src={addcart} alt="addcart" />
+              </Link>
+            </div>
+          </div>
+        </Link>
+      </div>
+      <div
+        className={` ${
+          location.pathname === "/store" ? `gr-${grid}` : "col-3"
+        } `}
+      >
+        <Link className="product-card position-relative">
+          <div className="wishlist-icon position-absolute">
+            <Link>
+              <img src={wish} alt="wishlist" />
+            </Link>
+          </div>
+          <div className="product-image">
+            <img src={watch} className="img-fluid" alt="product img" />
+            <img src={speaker} alt="product img" />
+          </div>
+          <div className="product-details">
+            <h6 className="brand">Havels</h6>
+            <h5 className="product-title">
+              Kids headphones bulk 10 pack multi colored for students
+            </h5>
+            <ReactStars
+              count={5}
+              size={24}
+              value={4}
+              edit={false}
+              activeColor="#ffa700"
+            />
+            <p className={`description ${grid === 12 ? "d-block" : "d-none"}`}>
+              At vero eos et accusamus et lusto odioGign1ss1mos ducxmus qul
+              blanditiis praesentium voluptatum deleniti atque corrupti quos
+              dolores et quas molestias excepturi sint occaecati cupiditate non
+              provident, similique sunt.
+            </p>
+            <p className="price">100.00$</p>
+          </div>
+          <div className="action-bar position-absolute">
+            <div className="d-flex flex-column gap-15">
+              <button className="border-0 bg-transparent">
+                <img src={prodcompare} alt="compare" />
+              </button>
+              <button className="border-0 bg-transparent">
+                <img src={view} alt="view" />
+              </button>
+              <button className="border-0 bg-transparent">
+                <img src={addcart} alt="addcart" />
+              </button>
+            </div>
+          </div>
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default ProductCard;
